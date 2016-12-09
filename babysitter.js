a3b38ddbfa901ce1a77b5813ff07bd068814467c
James Lindstrom <jimmyaeolus@gmail.com>
2016-12-08 21:08:21 -0500

    Clean up magic number

--- babysitter.js
+++ babysitter.js
@@ -1,7 +1,10 @@
 var babysit = {
-	//Converts a string the represents a time (eg. "5PM") to an hour number that will be used by the program, starting with 5 PM as 0.
-	timeStrToHourNum(timeStr){
-		var hours = parseInt(timeStr) - 5;
+	//5PM is the earliest time the babysitter will start.
+	earliestStart: 5,
+	
+	//Converts a string the represents a time (eg. "5PM") to an hour number that will be used by the program, starting with babysit.earliestStart as 0.
+	timeStrToHourNum : function(timeStr){
+		var hours = parseInt(timeStr) - babysit.earliestStart;
 		
 		//If hours are in the AM (and it is not 12AM), add 12.
 		if(timeStr.search('AM') > 0 && parseInt(timeStr) != 12){hours += 12}
