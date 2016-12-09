ddf333b11d5714b4386fe7740758729a3ba0a2d7
James Lindstrom <jimmyaeolus@gmail.com>
2016-12-08 22:10:53 -0500

    Revise unnecessarily complex code
    Previously I was subtracting the babysit.earliestTime from any times I converted from strings, thinking it would lead to a simpler and less confusing program. When I got to error handling, I realized I was incorrect. The tests have also been revised to be compatible with the changes.

--- babysitter.js
+++ babysitter.js
@@ -1,10 +1,11 @@
 var babysit = {
 	//5PM is the earliest time the babysitter will start.
+	//4AM is the latest time the babysitter will end.
 	earliestStart: 5,
 	
 	//Converts a string the represents a time (eg. "5PM") to an hour number that will be used by the program, starting with babysit.earliestStart as 0.
 	timeStrToHourNum : function(timeStr){
-		var hours = parseInt(timeStr) - babysit.earliestStart;
+		var hours = parseInt(timeStr);
 		
 		//If hours are in the AM (and it is not 12AM), add 12.
 		if(timeStr.search('AM') > 0 && parseInt(timeStr) != 12){hours += 12}
@@ -16,11 +17,9 @@ var babysit = {
 		var startTime = babysit.timeStrToHourNum(startTimeStr);
 		var endTime = babysit.timeStrToHourNum(endTimeStr);
 		var bedTime = babysit.timeStrToHourNum(bedTimeStr);
+		var midnight = 12;
 		
 		var dollars;
-		
-		//Midnight will be calculated in case babysit.earliestStart is changed in the future.
-		var midnight = babysit.timeStrToHourNum("12AM");
 		
 		//If the end time is after midnight, it must be factored into the calculation.
 		if(endTime > midnight){
--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 QUnit.test("Test 1", function(assert){
-	assert.equal(babysit.timeStrToHourNum('5PM'), 0, "5PM is the earliest possible hour and should return 0.");
+	assert.equal(babysit.timeStrToHourNum('5PM'), 5, "5PM should return 5.");
 });
 
 QUnit.test("Test 2", function(assert){
-	assert.equal(babysit.timeStrToHourNum('6PM'), 1, "At 6PM, 1 hour has passed since 5PM.");
-	assert.equal(babysit.timeStrToHourNum('7PM'), 2, "At 7PM, 2 hours have passed since 5PM.");
+	assert.equal(babysit.timeStrToHourNum('6PM'), 6, "6PM should return 6.");
+	assert.equal(babysit.timeStrToHourNum('7PM'), 7, "7PM should return 7.");
 });
 
 QUnit.test("Test 3", function(assert){
-	assert.equal(babysit.timeStrToHourNum('1AM'), 8, "At 1AM, 8 hours have passed since 5PM.");
-	assert.equal(babysit.timeStrToHourNum('2AM'), 9, "At 2AM, 9 hours have passed since 5PM.");
+	assert.equal(babysit.timeStrToHourNum('1AM'), 13, "1AM should return 13.");
+	assert.equal(babysit.timeStrToHourNum('2AM'), 14, "2AM should return 14.");
 });
 
 QUnit.test("Test 4", function(assert){
-	assert.equal(babysit.timeStrToHourNum('12AM'), 7, "At 12AM, 7 hours have passed since 5PM. 12AM is the only AM time when 12 hours do not need to be subtracted.");
+	assert.equal(babysit.timeStrToHourNum('12AM'), 12, "12AM should return 12. 12AM is the only AM time when 12 hours do not need to be subtracted.");
 });
 
 QUnit.test("Test 5", function(assert){
